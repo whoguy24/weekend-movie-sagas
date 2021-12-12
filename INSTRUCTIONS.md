fc072f43a5fc208df8148f70c2aa70529703b104
whoguy24 <whoguy24@gmail.com>
2021-12-12 11:17:57 -0600

    Added add movie button to movie list page.

--- INSTRUCTIONS.md
+++ INSTRUCTIONS.md
@@ -45,7 +45,7 @@ We've added some movie posters in the `public/images` folder, and the database i
 This view is completed already! It displays all of the movies in the movie database. 
 
 - TODO: When a movie poster is clicked, a user should be brought to the `/details` view for that movie. ✅
-- TODO: Have a way to get to the Add Movie Page
+- TODO: Have a way to get to the Add Movie Page ✅
 
 ### Details Page
 
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,6 +1,7 @@
 // IMPORT COMPONENTS
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import './MovieList.css'
 import MovieListItem from '../MovieListItem/MovieListItem.jsx'
 
@@ -9,6 +10,7 @@ function MovieList() {
 
     // Define Import Variables
     const dispatch = useDispatch();
+    const history = useHistory();
 
     // Define Redux Store Variables
     const movies = useSelector(store => store.movies);
@@ -18,10 +20,15 @@ function MovieList() {
         dispatch({ type: 'FETCH_MOVIES' });
     }, []);
 
+    function handleAddMovieButton () {
+        history.push('/add_movie');
+    }
+
     // Render on the DOM
     return (
         <main>
             <h1>MovieList</h1>
+            <button onClick={handleAddMovieButton}>Add Movie</button>
             <section className="movies">
                 {movies.map((movie) => (
                     <MovieListItem key={movie.id} movieItem={movie}/>
